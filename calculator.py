b94e2b4262203df18c86f0e33cd9168bbf7a3274
yogeshwaran <yogeshwaran05b@gmail.com>
2025-06-22 12:21:30 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,14 @@
+num1=int(input("enter the first  number:"))
+opertor=(input("enter the operator:"))
+num2=int(input("enter the second number:"))
+
+if opertor == "+":
+    print(num1 + num2)
+elif opertor == "-":
+    print(num1 - num2)
+elif opertor == "*":
+    print(num1 * num2)
+elif opertor == "%":
+    print(num1 % num2)
+elif opertor == "/":
+    print(num1 / num2)
